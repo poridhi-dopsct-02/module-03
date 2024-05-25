76362f09378132d8186cb59608e301a02b9a44c2
Rifat <rifat.islam173@northsouth.edu>
2024-05-25 14:48:17 +0600

    added pipenv

--- backend/flaskApp/main.py
+++ backend/flaskApp/main.py
@@ -7,12 +7,13 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["CORS_HEADERS"] = "Content-Type"
 
 mongo_db_url = os.environ.get("mongodb://localhost:27017/expense")
 
 client = MongoClient(mongo_db_url)
-db = client['expense']
+db = client["expense"]
+
 
 @app.post("/api/expense")
 @cross_origin()
@@ -25,46 +26,56 @@ def addExpense():
     resp.status_code = 200
     return resp
 
+
 @app.get("/api/expense")
-def get_sensors():
+@cross_origin()
+def get_expenses():
     expenses = list(db.expense.find())
-    
+
     print(expenses)
     transactions = []
     totalIncome = 0
     totalExpense = 0
 
     for expense in expenses:
-        if(float(expense["amount"])>0):
+        if float(expense["amount"]) > 0:
             totalIncome += expense["amount"]
         else:
             totalExpense += expense["amount"]
-            
-        
-        transactions.append({"id": str(expense["_id"]), 
-                            "title": expense["title"],
-                            "amount": expense["amount"]})
-    
-    transactions.reverse()
-    
-    responseData = {
-            "totalIncome": totalIncome,
-            "totalExpense": totalExpense,
-            "transactions" : transactions
-        }
 
+        transactions.append(
+            {
+                "id": str(expense["_id"]),
+                "title": expense["title"],
+                "amount": expense["amount"],
+            }
+        )
+
+    transactions.reverse()
+
+    responseData = {
+        "totalIncome": totalIncome,
+        "totalExpense": totalExpense,
+        "transactions": transactions,
+    }
 
     response = Response(
-        response=(dumps(responseData)), status=200,  mimetype="application/json")
+        response=(dumps(responseData)), status=200, mimetype="application/json"
+    )
     return response
 
 
 @app.delete("/api/expense/<recordId>")
+@cross_origin()
 def removeExpense(recordId):
-    if recordId == None: return {}
-    
+    if recordId == None:
+        return {}
+
     db.expense.delete_one({"_id": ObjectId(recordId)})
 
     resp = jsonify({"message": "Expense record deleted successfully"})
     resp.status_code = 200
     return resp
+
+
+app.run(port=8000)
